5e101f6d2baec15fb7144362c8daa385c6a695e2
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-23 07:47:22 -0500

    adding screens and movies

---
+++ booking.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import tkinter.ttk
+
+screens = [
+    'Screen 1', 'Screen 2', 'Screen 3',
+    'Screen 1', 'Screen 2', 'Screen 3',
+    'Screen 1', 'Screen 2', 'Screen 3',
+]
+
+movies = {
+    'Horror': [
+        'Hereditary', 
+        'A Quiet Place', 
+        'The Conjuring 2', 
+        'The Grudge',
+        'Anabelle Comes Home'
+    ],
+    'Action': [
+        'Avengers End Game',
+        'John Wick Chapter 3',
+        'Aquaman',
+        'Black Panther',
+        'Mission Impossible'
+    ],
+    'Drama': [
+        'Joker',
+        'Spotlight',
+        'Little Women',
+        'The Irishman',
+        'A Star is Born'
+    ],
+    'Comedy': [
+        'Step Brothers',
+        'BookSmart',
+        'Horrible Bosses',
+        'The Other Guys',
+        'SuperBad'
+    ],
+    'Sci-Fi': [
+        'The Fault in our Stars',
+        'The Notebook',
+        'The Tourist',
+        'Titanic',
+        'Crazy Rich Asians'
+    ]
+}
