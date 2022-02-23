2f3408e0b781c39b8c8691ee3ac6a7952f6a306b
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-23 07:54:46 -0500

    initializing seats

--- booking.py
+++ booking.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 import tkinter.ttk
 
+# adding the screens
 screens = [
     'Screen 1', 'Screen 2', 'Screen 3',
     'Screen 1', 'Screen 2', 'Screen 3',
     'Screen 1', 'Screen 2', 'Screen 3',
 ]
 
+# adding the movies
 movies = {
     'Horror': [
         'Hereditary', 
@@ -44,3 +46,12 @@ movies = {
         'Crazy Rich Asians'
     ]
 }
+
+# adding the times
+times = [
+    '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00',
+]
+
+# seats
+seatList = []
+seatSelected = []
